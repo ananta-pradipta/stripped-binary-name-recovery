15053a442a8b63e6614b78557c725f9325540365
ananta-pradipta <ananta.dito@gmail.com>
2026-08-26 01:44:26 -0400

    AUDIT FIX: dump_embeddings_v2/router_v2 did not pass enrich_a3/rodata_consts_dir -> A3+ retrieval/router reads were on un-enriched inputs; rerunning

--- scripts/router_v2.py
+++ scripts/router_v2.py
@@ -158,7 +158,9 @@ def main():
         max_blocks=cfg['data']['max_blocks_per_function'], max_tokens=cfg['data']['max_tokens_per_block'],
         max_name_len=cfg['data']['max_name_length'], min_tokens=cfg['data'].get('min_tokens', 1),
         token_vocab=ckpt['token_vocab'], ext_vocab=ckpt['ext_vocab'], vocab_binaries=set(split['train']),
-        cache_path=cfg['data'].get('cache_path'))
+        cache_path=cfg['data'].get('cache_path'),
+        enrich_a3=bool(cfg['data'].get('enrich_a3', False)),
+        rodata_consts_dir=cfg['data'].get('rodata_consts_dir'))
     sp = dataset.sp
     train_idx, val_idx, test_idx = dataset.get_splits(cfg['data']['train_split'], cfg['data']['val_split'],
                                                       split_file=cfg['data']['split_file'])
